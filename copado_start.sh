eeca07ced0a8aca84819f1f21ae1fe5c6c79a271
gaviriajesus <jgaviria@copa.do>
2019-01-22 10:00:40 +0100

    Update copado_start.sh

--- copado_start.sh
+++ copado_start.sh
@@ -8,3 +8,5 @@ notify_status "Retrieving%20locations" "25"
 echo $C1P_ORGCREDENTIALID_DEMO_ENDPOINT
 echo $C1P_ORGCREDENTIALID_DEMO_AUTH_HEADER
 
+
+notify_status "Sending%20JSON" "%7B%20%22name%22%3A%22John%22%2C%20%22age%22%3A30%2C%20%22car%22%3Anull%20%7D"
